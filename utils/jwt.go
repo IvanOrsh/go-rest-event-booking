85309237c0b60625f5f4a751686318a2f304e2e9
IvanOrsh <ksironov@mail.ru>
2023-12-23 02:05:20 +0300

    feat: implement verify jwt token

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -17,4 +18,33 @@ func GenerateToken(email string, userId int64) (string, error) {
 	})
 
 	return token.SignedString([]byte(secretKey))
+}
+
+func VerifyToken(tokenString string) error {
+	parsedToken, err := jwt.Parse(tokenString, func(token *jwt.Token) (any, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, jwt.ErrSignatureInvalid
+		}
+
+		return []byte(secretKey), nil
+	})
+
+	if err != nil {
+		return errors.New("could not parse token")
+	}
+
+	if !parsedToken.Valid {
+		return errors.New("invalid token")
+	}
+
+	// extract data from token: email, userId
+	// claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	// if !ok {
+	// 	return errors.New("invalid token claims")
+	// }
+
+	// email := claims["email"].(string)
+	// userId := claims["userId"].(float64)
+
+	return nil
 }
