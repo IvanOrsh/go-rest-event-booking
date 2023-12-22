a4efc9457340ee3f190a062d8cc30ffa80e6fd63
IvanOrsh <ksironov@mail.ru>
2023-12-22 21:57:21 +0300

    feat: fetch event by id

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 
@@ -15,6 +16,7 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
 
 	server.Run(":8080")
@@ -38,6 +40,38 @@ func getEvents(c *gin.Context) {
 			"message": "events retrieved",
 			"data":    events,
 		})
+}
+
+func getEvent(c *gin.Context) {
+	id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+	if err != nil {
+		c.JSON(
+			http.StatusBadRequest,
+			gin.H{
+				"message": "could not parse id",
+				"error":   err.Error(),
+			})
+		return
+	}
+
+	event, err := models.GetEventByID(id)
+	if err != nil {
+		c.JSON(
+			http.StatusInternalServerError,
+			gin.H{
+				"message": "could not retrieve event",
+				"error":   err.Error(),
+			})
+		return
+	}
+
+	c.JSON(
+		http.StatusOK,
+		gin.H{
+			"message": "event retrieved",
+			"data":    event,
+		},
+	)
 }
 
 func createEvent(c *gin.Context) {
